343d8a01e0daef3600886069d60a92efec0b97e6
Anderson <andersonwilliansantos100@gmail.com>
2026-06-01 13:23:27 -0300

    Corrige caminhos para GitHub Pages

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function toggleMode() {
 
   // substituir a imagem
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar_sol.png")
+    img.setAttribute("src", "assets/avatar_sol.png")
     img.setAttribute(
       "alt",
       "Desenho do sol usando óculos azuis, com a bochecha corada e sorrido.",
     )
   } else {
-    img.setAttribute("src", "./assets/avatar_lua.png")
+    img.setAttribute("src", "assets/avatar_lua.png")
     img.setAttribute(
       "alt",
       "Desenho da lua em um céu estrelado, com a bochecha corada e sorrido.",
